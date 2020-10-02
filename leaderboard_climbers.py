0d96ae9873f80630510c0cc3ca4fea67fc3c9a1e
LeonV-GIT <leonvisage@gmail.com>
2020-10-01 22:55:32 -0400

    commit

---
+++ leaderboard_climbers.py
@@ -0,0 +1,31 @@
+def leaderboard_sort(leaderboard, changes):
+    lb = leaderboard.copy()
+    for x in changes:
+        if " " in x:
+            holder = x.split(' ')
+            target = holder[0]
+            mv = holder[1]
+            moves = int(mv[-1])
+            direction = mv[0]
+            for i in range(moves):
+                t = lb.index(target)
+                if direction == "+":
+                    lb[t-1], lb[t] = lb[t], lb[t-1]
+                if direction == "-":
+                    lb[t], lb[t+1] = lb[t+1], lb[t]
+        else: 
+            pass
+    return lb
+
+        
+# Itterate over the changes list 
+# Split changes list string allong the " "  
+# Find the target in the leaderboard list
+# def move_up def move_down based on + or - in string
+# for x in range(last number of changes string)
+
+    
+
+
+
+leaderboard_sort(['John', 'Brian', 'Jim', 'Dave', 'Fred'], ['Dave +2', 'Fred +4', 'Brian -1'])
